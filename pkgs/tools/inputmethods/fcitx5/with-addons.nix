9ae2c81cee308fb927e0d61a60d089811e6fdce2
Kazutoshi Noguchi <noguchi.kazutosi+Az0Gjft4@gmail.com>
2022-09-25 15:40:11 +0900

    fcitx5: fix a bug which causes fcitx5 cannot start the configuration tool with 'Configure' menu

--- pkgs/tools/inputmethods/fcitx5/with-addons.nix
+++ pkgs/tools/inputmethods/fcitx5/with-addons.nix
@@ -1,4 +1,7 @@
-{ symlinkJoin, makeWrapper, fcitx5, fcitx5-lua, fcitx5-configtool, fcitx5-qt, fcitx5-gtk, addons ? [] }:
+{ lib, symlinkJoin, makeWrapper, fcitx5, fcitx5-lua, fcitx5-configtool, fcitx5-qt,
+  fcitx5-gtk, coreutils, which, gnugrep, kdialog, gettext, dbus, xprop, xdg-utils,
+  systemsettings,
+  addons ? [], kcmSupport ? true }:
 
 symlinkJoin {
   name = "fcitx5-with-addons-${fcitx5.version}";
@@ -7,11 +10,19 @@ symlinkJoin {
 
   nativeBuildInputs = [ makeWrapper ];
 
-  postBuild = ''
+  postBuild = let
+    configtool-path = lib.makeBinPath (
+      [ coreutils which gnugrep kdialog gettext dbus xprop fcitx5-configtool xdg-utils ] ++
+      lib.optionals kcmSupport [ systemsettings ]
+    );
+  in ''
+    # fcitx5 starts ''${fcitx5}/bin/fcitx5-configtool instead of the tool in the $PATH
     wrapProgram $out/bin/fcitx5 \
       --prefix FCITX_ADDON_DIRS : "$out/lib/fcitx5" \
       --suffix XDG_DATA_DIRS : "$out/share" \
-      --suffix PATH : "$out/bin"
+      --suffix PATH : "$out/bin:${configtool-path}"
+
+    wrapProgram $out/bin/fcitx5-configtool --prefix PATH ":" "${configtool-path}"
 
     desktop=share/applications/org.fcitx.Fcitx5.desktop
     autostart=etc/xdg/autostart/org.fcitx.Fcitx5.desktop
